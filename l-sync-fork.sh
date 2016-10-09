484b7816c65ffbad061ffbd2104a5cd7833c368e
sbts <info@sbts.com.au>
2016-10-09 23:49:20 +0800

    make sync-fork use the version in this repo instead of the one in the target repo

--- l-sync-fork.sh
+++ l-sync-fork.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-Dir=`git rev-parse --show-toplevel`
+#Dir=`git rev-parse --show-toplevel`
 
-exec "$Dir/utils/devel/sync-fork.sh"
+#exec "$Dir/utils/devel/sync-fork.sh"
+exec "$Real"
 
 exit
